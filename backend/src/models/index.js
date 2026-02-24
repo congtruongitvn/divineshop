15a567be35183a13876bd1fd2dd88382c7f02224
congtruongitvn <55949169+congtruongitvn@users.noreply.github.com>
2026-02-24 16:36:23 +0700

    Fix DB

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'divineshop',
+  process.env.DB_NAME || 'postgres',
   process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || 'postgres',
+  process.env.DB_PASSWORD || 'vrlhcxl8',
   { host: process.env.DB_HOST || 'localhost', port: process.env.DB_PORT || 5432,
     dialect: 'postgres', logging: false }
 );
