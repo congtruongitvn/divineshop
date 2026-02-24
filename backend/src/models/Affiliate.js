815fe0c0f772789a408e08441d9c84ebe6d51843
congtruongitvn <55949169+congtruongitvn@users.noreply.github.com>
2026-02-24 17:26:41 +0700

    fix: remove duplicate sequelize imports in all model files

--- backend/src/models/Affiliate.js
+++ backend/src/models/Affiliate.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('./index');
+
 const sequelize = require('./db');
 
 const Affiliate = sequelize.define('Affiliate', {
--- backend/src/models/DigitalKey.js
+++ backend/src/models/DigitalKey.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('./index');
+
 const sequelize = require('./db');
 
 const DigitalKey = sequelize.define('DigitalKey', {
